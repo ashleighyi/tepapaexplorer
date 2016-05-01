0b7d807b0e855fb579d16a1e53ab6a781e48bafd
ashleighyi <ashweiiq@gmail.com>
2016-05-01 23:28:36 +1200

    yaya

---
+++ js/app.js
@@ -0,0 +1,31 @@
+	//firebase reference
+	var data = new Firebase('https://tepapaexplorer.firebaseio.com/');
+
+
+	//on change get data 
+	data.on("value", function(snapshot) {
+		context = snapshot.val();
+
+		var source = $("#home-template").html ();
+		var template = Handlebars.compile(source);
+		var html = template(context);
+
+		console.log(html);
+		$('#yield').html(html);
+	});
+
+	//Event Method
+	$("#submit").click(function(){
+		
+		var book = {
+			exhibition: null,
+			about: null,
+			where: null,
+			cost: null,
+		};
+
+		book.exhibition = $("#exhibition").val();
+		book.about = $("#about").val();
+
+		data.child("books").push(book);
+	});
